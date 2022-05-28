f0140e8f6eab8d223a82c8b4991bee28caccc8ac
FrancescaVV <francesca.voidoc@gmail.com>
2022-05-28 23:14:09 +0300

    finished register and login functionality

---
+++ app/src/main/java/com/example/sportssdbis/DashboardUserActivity.java
@@ -0,0 +1,54 @@
+package com.example.sportssdbis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.sportssdbis.databinding.ActivityDashboardUserBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class DashboardUserActivity extends AppCompatActivity {
+
+    //view binding
+    private ActivityDashboardUserBinding binding;
+
+    //firebase auth
+    private FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDashboardUserBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUser();
+
+        //logout
+        binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                checkUser();
+            }
+        });
+    }
+    private void checkUser() {
+        //get current user
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser == null){
+            //not logged in, go to main screen
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+        else {
+            //logged in, get info
+            String email = firebaseUser.getEmail();
+            //set in textview toolbar
+            binding.subTitleTv.setText(email);
+        }
+    }
+}
