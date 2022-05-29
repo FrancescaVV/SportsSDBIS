031ba1cba7f1f01e0e5e508007635c1d9fd24bdd
FrancescaVV <francesca.voidoc@gmail.com>
2022-05-29 13:35:31 +0300

    categories are shown in admin dashboard

--- app/src/main/java/com/example/sportssdbis/DashboardAdminActivity.java
+++ app/src/main/java/com/example/sportssdbis/DashboardAdminActivity.java
@@ -1,5 +1,6 @@
 package com.example.sportssdbis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,6 +10,13 @@ import android.view.View;
 import com.example.sportssdbis.databinding.ActivityDashboardAdminBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class DashboardAdminActivity extends AppCompatActivity {
 
@@ -17,6 +25,9 @@ public class DashboardAdminActivity extends AppCompatActivity {
 
     //firebase auth
     private FirebaseAuth firebaseAuth;
+
+    private ArrayList<ModelCategory> categoryArrayList;
+    private AdapterCategory adapterCategory;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +37,7 @@ public class DashboardAdminActivity extends AppCompatActivity {
         //init firebase auth
         firebaseAuth = FirebaseAuth.getInstance();
         checkUser();
+        loadCategories();
 
         //logout
         binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +53,35 @@ public class DashboardAdminActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(DashboardAdminActivity.this, CategoryAddActivity.class));
+            }
+        });
+    }
+
+    private void loadCategories(){
+        categoryArrayList = new ArrayList<>();
+        //get all categories from db
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Categories");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //clear
+                categoryArrayList.clear();
+                for (DataSnapshot ds:snapshot.getChildren()){
+                    //get data
+                    ModelCategory model = ds.getValue(ModelCategory.class);
+
+                    //add to array list
+                    categoryArrayList.add(model);
+                }
+
+                adapterCategory = new AdapterCategory(DashboardAdminActivity.this, categoryArrayList);
+
+                binding.categoriesRv.setAdapter(adapterCategory);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
---
+++ app/src/main/java/com/example/sportssdbis/ModelCategory.java
@@ -0,0 +1,33 @@
+package com.example.sportssdbis;
+
+public class ModelCategory {
+
+    String id, category, uid;
+    long timestamp;
+
+    public ModelCategory() {
+    }
+
+    public ModelCategory(String id, String category, String uid, long timestamp) {
+        this.id = id;
+        this.category = category;
+        this.uid = uid;
+        this.timestamp = timestamp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
